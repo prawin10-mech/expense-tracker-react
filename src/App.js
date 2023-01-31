4ce71c3254be4f7ba1a800fab82060e6f4c7817b
prawin10-mech <pra6650@gmail.com>
2023-01-31 11:07:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import ExpenseItems from "./components/ExpenseItems";
+
+function App() {
+  return (
+    <>
+      <h1>Expense Items</h1>
+      <ExpenseItems />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+function ExpenseItems() {
+  return (
+    <>
+      <h2>Food Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
+    </>
+  );
+}
+
+export default ExpenseItems;
