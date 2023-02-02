8cf3bcce4c07d3ee2e61d4eeb8b13a6b87f0cafa
prawin10-mech <pra6650@gmail.com>
2023-02-02 17:20:19 +0530

    used useState hook

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,22 +1,35 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
-const ExpenseItems = ({ id, title, date, amount, LocationOfExpenditure }) => {
+const ExpenseItems = (props) => {
+  let [title, setTitle] = useState(props.title);
+  let [amount, setAmount] = useState(props.amount);
   const handleDelete = (e) => {
     const childNode = e.target.parentNode;
     const parentNode = childNode.parentNode;
     parentNode.removeChild(childNode);
     console.log(childNode);
   };
+
+  const handleTitle = () => {
+    setTitle("update");
+  };
+
+  const handleAmount = () => {
+    setAmount(100);
+  };
   return (
-    <div className="expense-item" key={id}>
-      <ExpenseDate date={date} />
+    <div className="expense-item" key={props.id}>
+      <ExpenseDate date={props.date} />
       <ExpenseDetails
         title={title}
         amount={amount}
-        LocationOfExpenditure={LocationOfExpenditure}
+        LocationOfExpenditure={props.LocationOfExpenditure}
       />
+      <button onClick={handleAmount}>Update Amount</button>
+      <button onClick={handleTitle}>Update Title</button>
       <button onClick={handleDelete}>Delete</button>
     </div>
   );
