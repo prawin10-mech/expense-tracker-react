4cbe28f9117683fcf9ee2e41a0d5f4d3f64ab44d
prawin10-mech <pra6650@gmail.com>
2023-02-02 09:23:29 +0530

    looped all the expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Movie veerayya",
+      amount: 299,
+      date: new Date(2023, 0, 23),
+      LocationOfExpenditure: "tanuku",
+    },
+    {
+      id: 2,
+      title: "cable bill",
+      amount: 550,
+      LocationOfExpenditure: "meenavilluru",
+      date: new Date(2023, 0, 30),
+    },
+    {
+      id: 3,
+      title: "Tiffin",
+      amount: 10,
+      date: new Date(2023, 1, 02),
+      LocationOfExpenditure: "ramachandra puram",
+    },
+    {
+      id: 3,
+      title: "Cake",
+      amount: 30,
+      date: new Date(2023, 0, 31),
+      LocationOfExpenditure: "ramachandra puram",
+    },
+  ];
   return (
     <>
       <h1>Expense Items</h1>
-      <ExpenseItems />
+      {expenses.map((expense) => {
+        return <ExpenseItems {...expense} />;
+      })}
     </>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
-function ExpenseItems() {
-  const date = new Date();
-  const description = "cable bill";
-  const price = 25;
-  const LocationOfExpenditure = "tanuku";
+function ExpenseItems({ title, date, amount, LocationOfExpenditure }) {
   return (
     <div className="expense-item">
       <div>{date.toISOString()}</div>
       <div>{LocationOfExpenditure}</div>
       <div className="expense-item__description">
-        <h2>{description}</h2>
-        <div className="expense-item__price">{price}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
