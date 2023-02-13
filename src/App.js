bd8a5f9aa806330b2d80f7f2472d2550e28bf398
prawin10-mech <pra6650@gmail.com>
2023-02-13 18:24:35 +0530

    usestate in app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
+import { useState } from "react";
 import ExpenseItems from "./components/Expenses/ExpenseItems";
 import "../public/app.css";
 import Card from "./components/UI/Card";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 0,
+    title: "Movie veerayya",
+    amount: 299,
+    date: new Date(2023, 0, 23),
+    LocationOfExpenditure: "tanuku",
+  },
+  {
+    id: 2,
+    title: "cable bill",
+    amount: 550,
+    LocationOfExpenditure: "meenavilluru",
+    date: new Date(2023, 0, 30),
+  },
+  {
+    id: 3,
+    title: "Tiffin",
+    amount: 10,
+    date: new Date(2023, 1, 02),
+    LocationOfExpenditure: "ramachandra puram",
+  },
+  {
+    id: 3,
+    title: "Cake",
+    amount: 30,
+    date: new Date(2023, 0, 31),
+    LocationOfExpenditure: "ramachandra puram",
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: 0,
-      title: "Movie veerayya",
-      amount: 299,
-      date: new Date(2023, 0, 23),
-      LocationOfExpenditure: "tanuku",
-    },
-    {
-      id: 2,
-      title: "cable bill",
-      amount: 550,
-      LocationOfExpenditure: "meenavilluru",
-      date: new Date(2023, 0, 30),
-    },
-    {
-      id: 3,
-      title: "Tiffin",
-      amount: 10,
-      date: new Date(2023, 1, 02),
-      LocationOfExpenditure: "ramachandra puram",
-    },
-    {
-      id: 3,
-      title: "Cake",
-      amount: 30,
-      date: new Date(2023, 0, 31),
-      LocationOfExpenditure: "ramachandra puram",
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("hello");
-    console.log(expense);
-    expenses.push(expense);
+    setExpenses([...expenses, expense]);
   };
   return (
     <>
