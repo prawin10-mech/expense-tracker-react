41321e14b17579127a58b552ff0f3a727ed4f5cf
prawin10-mech <pra6650@gmail.com>
2023-02-13 16:58:45 +0530

    added usestate

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,22 +1,34 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 const ExpenseForm = () => {
-  const handleChange = (e) => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+    console.log(e.target.value);
+  };
+  const sateChangeHandler = (e) => {
+    setDate(e.target.value);
     console.log(e.target.value);
   };
 
   return (
-    <form onChange={handleChange}>
+    <form>
       <div className="new-form__controls">
         <div className="new-form__control">
           <label>Title: </label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
-        <div className="new-form__control" onChange={handleChange}>
+        <div className="new-form__control" onChange={amountChangeHandler}>
           <label>Amount: </label>
           <input type="number" min="0.01" step="0.01" />
         </div>
-        <div className="new-form__control" onChange={handleChange}>
+        <div className="new-form__control" onChange={sateChangeHandler}>
           <label>Date</label>
           <input type="date" min="2020-01-01" max="2023-02-01" />
         </div>
